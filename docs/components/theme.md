f246cd1a2b2f0480f9b6e77be3579c8704d6e1d6
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-06-22 15:03:08 +0600

    ⚰️ remove unnecessary files

--- docs/components/theme.md
+++
@@ -1,7 +0,0 @@
----
-id: theme
-title: Theme
-description: theme For React Native
----
-
-- [react-native-paper ★2218](https://github.com/callstack/react-native-paper) - Material design for React Native
--- sidebars.js
+++ sidebars.js
@@ -1,19 +1,3 @@
 module.exports = {
-  packages: [
-    "getting-started",
-    {
-      type: "category",
-      label: "Components",
-      items: [
-        "components/theme",
-        "components/icons",
-        "components/UI",
-        "components/styling",
-        "components/animation",
-        "components/libraries",
-        "components/frameworks",
-      ],
-    },
-    "forms",
-  ],
+  packages: ["getting-started", "forms"],
 };
