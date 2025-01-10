860415241f313d2144508b3b0a01d5ed75c2c77c
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2025-01-10 20:41:03 +0600

    🔧 update middleware to redirect all the links

--- middleware.ts
+++ middleware.ts
@@ -2,24 +2,61 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
+// Define a mapping of old paths to new URLs
+const redirects: { [key: string]: string } = {
+  "/onboardings": "/products?category=Onboarding",
+  "/carousels": "/products?category=Carousels",
+  "/parallaxes": "/products?category=Parallaxes",
+  "/dropdowns": "/products?category=Drop%20Down",
+  "/pickers": "/products?category=Pickers",
+  "/headers": "/products?category=Headers",
+  "/lists": "/products?category=List",
+  "/arcsliders": "/products?category=Arc%20Sliders",
+  "/sliders": "/products?category=Sliders",
+  "/tabbars": "/products?category=Tab%20bars",
+  "/bottomsheets": "/products?category=Bottom%20Sheets",
+  "/loaders": "/products?category=Loaders",
+  "/circular-progress-bars": "/products?category=Circular%20Progress%20Bars",
+  "/charts": "/products?category=Charts",
+  "/buttons": "/products?category=Buttons",
+  "/accordions": "/products?category=Accordions",
+  "/miscellaneous": "/products?category=Misc",
+  "/fullapps": "/products?category=Full%20Apps",
+}
+
 export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname
+  const { pathname } = request.nextUrl
 
-  if (pathname.startsWith("/onboardings")) {
-    return NextResponse.redirect(
-      new URL("/products?category=Onboarding", request.url)
-    )
+  // Use Object.hasOwn to check for property existence
+  if (Object.hasOwn(redirects, pathname)) {
+    const destination = redirects[pathname]
+    return NextResponse.redirect(new URL(destination, request.url))
   }
 
-  if (pathname.startsWith("/another-old-page")) {
-    return NextResponse.redirect(new URL("/another-new-page", request.url))
-  }
-
-  // Add more redirection rules as needed
-
+  // If no redirect is matched, proceed as normal
   return NextResponse.next()
 }
 
+// Specify the paths the middleware should run on
 export const config = {
-  matcher: "/:path*", // Apply middleware to all routes
+  matcher: [
+    "/onboardings",
+    "/carousels",
+    "/parallaxes",
+    "/dropdowns",
+    "/pickers",
+    "/headers",
+    "/lists",
+    "/arcsliders",
+    "/sliders",
+    "/tabbars",
+    "/bottomsheets",
+    "/loaders",
+    "/circular-progress-bars",
+    "/charts",
+    "/buttons",
+    "/accordions",
+    "/miscellaneous",
+    "/fullapps",
+  ],
 }
