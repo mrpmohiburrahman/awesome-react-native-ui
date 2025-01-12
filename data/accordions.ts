6ae66f4667cbdd94e723674ad41f70dbade35e65
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2025-01-12 15:55:36 +0600

    ✨ add accordion from William Candillon

--- data/accordions.ts
+++ data/accordions.ts
@@ -2,6 +2,20 @@ import type { ItemType } from "./items"
 
 export const accordions: ItemType[] = [
   {
+    id: "01JHCYVTTEX13MBTN0J187Z4EQ",
+    caption: "Accordion",
+    videoSrc: "demo/accordions/accordion_william_candillon.mp4",
+    thumbnailSrc: "thumbnails/accordions/accordion_william_candillon.jpg",
+    author: "William Candillon",
+    source:
+      "https://github.com/wcandillon/can-it-be-done-in-react-native/tree/master/reanimated-2/src/Accordion",
+    twitterId: "wcandillon",
+    linkedInId: "wcandillon",
+    githubId: "wcandillon",
+    category: "Misc",
+    created_at: "2025-01-12T09:52:36.042Z",
+  },
+  {
     id: "01JFF8MZX970G22KKR06AEM9K3",
     caption: "Dynamic Accordion",
     videoSrc: "demo/accordions/dynamic_accordion_hewad_mubariz.mp4",
